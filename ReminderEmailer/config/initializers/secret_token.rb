70f65a61ab2d0af578cedb7721e288e67f573510
Eric Brown <eric.d.brown23@gmail.com>
2013-03-16 00:17:32 -0500

    Initial Project Commit

---
+++ ReminderEmailer/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ReminderEmailer::Application.config.secret_token = '0c39fd532978ebbef20047c96fb4a71009e287d3048a3e47cc45ac5983e74a7bb82a9ddecc9fb4caa67d06ec7ea2675dd011318591cc7c335f2c710b9c9655e4'
