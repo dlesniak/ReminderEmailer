5a0a57784d1002426dbcf18c6aa1c7650193a13a
Eric Brown <eric.d.brown23@gmail.com>
2013-04-04 23:21:21 -0500

    Added some base failing tests, implemented basic edits

--- ReminderEmailer/app/assets/javascripts/reminders.js
+++ ReminderEmailer/app/assets/javascripts/reminders.js
@@ -1,3 +1,5 @@
+// There has to be a better way then keeping this global around
+var clicked_event;
 $(document).ready(function() {
   $('#calendar').fullCalendar({
     editable: true,
@@ -5,6 +7,7 @@ $(document).ready(function() {
       {
         url: '/reminders.json',
         type: 'GET',
+        cache: true,
         error: function() {
           alert("There was an error loading events!");
         }
@@ -14,11 +17,14 @@ $(document).ready(function() {
       alert(date + ' has been clicked!');
     },
     eventClick: function(event, jsEvent, view) {
+      // Modify the action of the form to reflect the clicked events id
+      $('#edit_reminder_form').attr('action', '/reminders/' + event.id + '/')
       // Fill in the form with data from the event
       $('#edit_reminder_title').val(event.title);
       $('#edit_reminder_start').val(event.start);
       $('#edit_reminder_end').val(event.end);
       $('#editReminder').modal('show');
+      clicked_event = event;
     },
     eventDrop: function(event, dayDelta, minuteDelta, allDay, revertFunc, jsEvent, ui, view) {
       alert(dayDelta);
@@ -32,12 +38,41 @@ $(document).ready(function() {
     }
   });
 
-  $('.date_picker_field').datetimepicker();
+  $('.date_picker_field').datetimepicker({
+    dateFormat: 'D M d yy',
+    timeFormat: 'HH:mm:ss z'
+  });
 
   $('#save_reminder_link').on('click', function(e) {
     // stop the default linking behavior
     e.preventDefault();
     // submit the form
     $('#new_reminder_form').submit();
+  });
+
+  // More DRY violations
+  $('#edit_reminder_link').on('click', function(e) {
+    // stop the default linking behavior
+    e.preventDefault();
+    // submit the form
+    var form = $('#edit_reminder_form');
+    var sub_data = form.serialize();
+    $.ajax({
+      url: form.attr('action'),
+      type: 'PUT',
+      data: sub_data,
+      dataType: "JSON"
+    }).success(function(json){
+      // refresh the data for the event
+      clicked_event.start = json.start;
+      clicked_event.end = json.end;
+      clicked_event.title = json.title;
+      //clicked_event.repeat = sub_data.repeat;
+      $('#calendar').fullCalendar('updateEvent', clicked_event);
+    });
+  });
+
+  $('.edit_close').on('click', function(e) {
+    clicked_event = null;
   });
 });
--- ReminderEmailer/app/models/reminder.rb
+++ ReminderEmailer/app/models/reminder.rb
@@ -1,3 +1,13 @@
 class Reminder < ActiveRecord::Base
   belongs_to :user
+
+  def self.in_range(start_datetime, end_datetime)
+    # Need to filter by user as well probably
+    start_datetime = Time.at(start_datetime).to_datetime
+    end_datetime = Time.at(end_datetime).to_datetime
+    # This could probably be optimized to one query...
+    start_reminders = Reminder.find(:all, :conditions => {:start => start_datetime..end_datetime})
+    end_reminders = Reminder.find(:all, :conditions => {:end => start_datetime..end_datetime})
+    @reminders = (start_reminders + end_reminders).uniq
+  end
 end
