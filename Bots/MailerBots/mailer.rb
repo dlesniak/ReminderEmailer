0914f06813f77a24b78132b5e126b1b3d15fe5a6
Eric Brown <eric.d.brown23@gmail.com>
2013-04-23 22:07:07 -0500

    minor bot changes

--- Bots/MailerBots/mailer.rb
+++ Bots/MailerBots/mailer.rb
@@ -6,8 +6,8 @@ uri = URI('http://localhost:3000')
 Net::HTTP.start('localhost', 3000, 'localhost', 8888) do |http|
   unixStart = (Time.now - 2.62974e6)
   unixEnd = (Time.now)
-  # request = Net::HTTP::Get.new('/api/v1/reminders?start=' + (unixStart.to_i.to_s) + '&end=' + (unixEnd.to_i.to_s))
-  request = Net::HTTP::Get.new('/api/v1/users/1/')
+  request = Net::HTTP::Get.new('/api/v1/reminders?start=' + (unixStart.to_i.to_s) + '&end=' + (unixEnd.to_i.to_s))
+  # request = Net::HTTP::Get.new('/api/v1/users/1/')
   # set the authorization header in the request
   request['Authorization'] = 'edb8ba0599a76b67e8769eb1e7b4b4d7' # a mailer key
   # request['Authorization'] = '3591751c803c7a4c8a39ce043f815623' # a user key
